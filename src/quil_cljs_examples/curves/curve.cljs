5296ecd0c1632bb9e811a6957d856378833c287c
Norgat <norg113@gmail.com>
2014-06-09 11:13:08 +0400

    Add curve-point, curve-tangent to curve/curve.cljx example.

--- src/quil_cljs_examples/curves/curve.cljs
+++ src/quil_cljs_examples/curves/curve.cljs
@@ -5,12 +5,19 @@
 
 (defn draw []
 	(q/background 255)
-	(q/stroke 255 102 0)
-	(q/curve 5 26 5 26 73 23 73 61)
+	(q/no-fill)
 	(q/stroke 0)
 	(q/curve 5 26 73 24 73 61 15 65)
-	(q/stroke 255 102 0)
-	(q/curve 73 24 73 61 15 65 15 65))
+	(doseq [i (range 6)]
+		(let [t (/ i 6.0)
+			  x  (q/curve-point 5 73 73 15 t)
+			  y  (q/curve-point 26 24 61 65 t)
+			  tx (q/curve-tangent 5 73 73 15 t)
+			  ty (q/curve-tangent 26 24 61 65 t)
+			  a  (- (q/atan2 ty tx) q/HALF_PI)
+			  x2 (+ x (* 8 (q/cos a)))
+			  y2 (+ y (* 8 (q/sin a)))]
+			  (q/line x y x2 y2))))
 
 
 (defsketch curve
