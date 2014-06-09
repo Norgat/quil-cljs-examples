c6c333e484a5150e06c4827cdef7a70902d4f633
Norgat <norg113@gmail.com>
2014-06-09 10:46:31 +0400

    Add bezier-point to curve/bezier.cljs example.

--- src/quil_cljs_examples/curves/bezier.cljs
+++ src/quil_cljs_examples/curves/bezier.cljs
@@ -7,7 +7,13 @@
 	(q/background 255)
 	(q/no-fill)
 	(q/stroke 0 0 0)
-	(q/bezier 85 20 10 10 90 90 15 80))
+	(q/bezier 85 20 10 10 90 90 15 80)
+
+	(q/fill 255)
+	(doseq [i (range 10)]
+		(q/ellipse (q/bezier-point 85 10 90 15 (/ i 10.0))
+				   (q/bezier-point 20 10 90 80 (/ i 10.0))
+				   5 5)))
 
 
 (defsketch bezier
