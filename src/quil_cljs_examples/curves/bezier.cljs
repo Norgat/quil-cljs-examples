92f02add50f55217674b218a9958ec4c56f1f03d
Norgat <norg113@gmail.com>
2014-06-09 10:57:30 +0400

    Add bezier-tangent to curve/bezier.cljs example.

--- src/quil_cljs_examples/curves/bezier.cljs
+++ src/quil_cljs_examples/curves/bezier.cljs
@@ -11,9 +11,17 @@
 
 	(q/fill 255)
 	(doseq [i (range 10)]
-		(q/ellipse (q/bezier-point 85 10 90 15 (/ i 10.0))
-				   (q/bezier-point 20 10 90 80 (/ i 10.0))
-				   5 5)))
+		(let [x  (q/bezier-point 85 10 90 15 (/ i 10.0))
+			  y  (q/bezier-point 20 10 90 80 (/ i 10.0))
+			  tx (q/bezier-tangent 85 19 90 15 (/ i 10.0))
+			  ty (q/bezier-tangent 20 10 90 80 (/ i 10.0))
+			  a  (+ q/PI (q/atan2 ty tx))
+			  x2 (+ x (* 30 (q/cos a)))
+			  y2 (+ y (* 30 (q/sin a)))]
+			  (q/stroke 255 102 0)
+			  (q/line x y x2 y2)
+			  (q/stroke 0)
+			  (q/ellipse x y 5 5))))
 
 
 (defsketch bezier
