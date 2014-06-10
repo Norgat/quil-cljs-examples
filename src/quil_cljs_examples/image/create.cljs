726a2a94bc50a63ee1024f58688cce2cd26a0264
Norgat <norg113@gmail.com>
2014-06-10 17:39:40 +0400

    Add copy() call.

--- src/quil_cljs_examples/image/create.cljs
+++ src/quil_cljs_examples/image/create.cljs
@@ -3,15 +3,20 @@
 	(:use-macros [quil.core :only [defsketch with-graphics]]))
 
 (def img (atom nil))
+(def new-img (atom nil))
 
 (defn setup []
 	(reset! img (q/create-image 50 50 :rgb))
-	(q/set-pixel @img 10 10 (q/color 0 90 102)))
+	(q/set-pixel @img 10 10 (q/color 0 90 102))
+
+	(reset! new-img (q/create-image 50 50 :rgb))
+	(q/copy @img @new-img [0 0 50 50] [0 0 50 50]))
 
 
 (defn draw []
 	(q/background 255)
-	(q/image @img 20 20))
+	(q/image @img 20 20)
+	(q/image @new-img 120 120))
 
 
 (defsketch create-image
