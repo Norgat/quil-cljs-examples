8961494dcf5fc7eb8aac0e78c7927bdf9c78480d
Norgat <norg113@gmail.com>
2014-06-11 12:48:12 +0400

    Add image blend example.

---
+++ src/quil_cljs_examples/image/blend.cljs
@@ -0,0 +1,30 @@
+(ns quil-cljs-examples.image.blend
+	(:require [quil.core :as q])
+	(:use-macros [quil.core :only [defsketch with-graphics]]))
+
+(def gr (atom nil))
+
+(defn setup []
+	(let [g (q/create-graphics 50 50 :p2d)]
+		(with-graphics g
+		  (q/background 40 200 255 200)
+		  (q/fill 255 0 0)
+		  (q/ellipse 12 12 20 20)
+		  (q/fill 0 255 0)
+		  (q/ellipse 38 12 20 20)
+		  (q/fill 0 0 255)
+		  (q/ellipse 25 38 20 20))
+		(reset! gr g)))
+
+
+(defn draw []
+	(q/background 255)
+	(q/image @gr 0 0)
+	(q/blend 0 0 300 300 20 20 200 200 :add))
+
+
+(defsketch image-blend
+	:host "image-blend"
+	:draw draw
+	:setup setup
+	:size [300 300])
