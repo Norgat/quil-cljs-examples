aa0b60dcd19128e5e8a48e26e7626dde0912eed0
Norgat <norg113@gmail.com>
2014-06-13 15:43:08 +0400

    Add typography examples.

---
+++ src/quil_cljs_examples/typography/attributes.cljs
@@ -0,0 +1,43 @@
+(ns quil-cljs-examples.typography.attributes
+	(:require [quil.core :as q])
+	(:use-macros [quil.core :only [defsketch with-graphics]]))
+
+(def lines "LIN1\nLIN2\nLIN3")
+
+(defn text-align []
+	(q/text-align :right)
+	(q/text "word" 30 30)
+
+	(q/text-mode :screen)
+	(q/text-align :center)
+	(q/text "word" 30 50)
+
+	(q/text-size 14)
+	(q/text-align :left)
+	(q/text "word" 30 70)
+	(q/text-size 10))
+
+(defn text-leading []
+	(q/text-leading 10)
+	(q/text lines 60 30)
+
+	(q/text-leading 20)
+	(q/text lines 90 30)
+
+	(q/text-leading 30)
+	(q/text lines 120 30))
+
+(defn draw []
+	(q/background 255)
+	(q/fill 0)
+	(q/text-leading 10)
+	(q/text-mode :model)
+
+	(text-align)
+	(text-leading))
+
+
+(defsketch typography-attributes
+	:host "typography-attributes"
+	:draw draw
+	:size [300 300])
---
+++ src/quil_cljs_examples/typography/metrics.cljs
@@ -0,0 +1,34 @@
+(ns quil-cljs-examples.typography.metrics
+	(:require [quil.core :as q])
+	(:use-macros [quil.core :only [defsketch with-graphics]]))
+
+
+(defn draw []
+	(q/background 255)
+	(q/fill 0)
+
+	(q/text-size 32)
+	(q/text "dp" 0 70)
+	(q/line 0 (- 70 (q/text-ascent))
+		    100 (- 70 (q/text-ascent)))
+
+	(q/text-size 64)
+	(q/text "dp" 35 70)
+	(q/line 35 (- 70 (q/text-ascent))
+		    100 (- 70 (q/text-ascent)))
+
+	(q/text-size 32)
+	(q/text "dp" 150 70)
+	(q/line 150 (+ 70 (q/text-descent))
+		    250 (+ 70 (q/text-descent)))
+
+	(q/text-size 64)
+	(q/text "dp" 185 70)
+	(q/line 185 (+ 70 (q/text-descent))
+		    250 (+ 70 (q/text-descent))))
+
+
+(defsketch typography-metrics
+	:host "typography-metrics"
+	:draw draw
+	:size [300 300])
