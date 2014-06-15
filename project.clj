edb429d0f36054a675be8d9a5c545c6bda95b580
Norgat <norg113@gmail.com>
2014-06-15 12:33:54 +0400

    Update deps versions.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
   				      [org.clojars.norgat/quil "2.0.0-SNAPSHOT"]
-  				      [org.clojure/clojurescript "0.0-2202"]]
+  				      [org.clojure/clojurescript "0.0-2234"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
   :hooks [leiningen.cljsbuild]
