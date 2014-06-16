726c05eab0267800a5a7dabebd98611301f34d05
Norgat <norg113@gmail.com>
2014-06-16 19:57:07 +0400

    Alpha version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-  				      [org.clojars.norgat/quil "2.0.0-SNAPSHOT"]
+  				      [org.clojars.norgat/quil "2.0.0-alpha"]
   				      [org.clojure/clojurescript "0.0-2234"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
