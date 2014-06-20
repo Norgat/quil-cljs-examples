15bbe6da85a3be906fe2da1d42feb5edb35036ab
Norgat <norg113@gmail.com>
2014-06-20 16:09:26 +0400

    Add middleware example.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject quil-cljs-examples "0.1.0-SNAPSHOT"
+(defproject quil-cljs-examples "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-  				      [org.clojars.norgat/quil "2.0.0-SNAPSHOT"]
+  				      [org.clojars.norgat/quil "2.0.1-SNAPSHOT"]
   				      [org.clojure/clojurescript "0.0-2234"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
---
+++ src/quil_cljs_examples/middleware.cljs
@@ -0,0 +1,19 @@
+(ns quil-cljs-examples.middleware
+	(:require [quil.core :as q]
+			  [quil.middleware :as m])
+	(:use-macros [quil.sketch :only [with-sketch]]
+				 [quil.core :only [defsketch]]))
+
+(defn setup []
+	{:x 100 :y 100})
+
+(defn draw [state]
+	(q/background 255)
+	(q/line 0 0 (:x state) (:y state)))
+
+(defsketch middleware
+	:draw draw
+	:setup setup
+	:size [300 300]
+	:host "middleware"
+	:middleware [m/fun-mode])
